73222ebdc601babf5fe58d0547ba0d75e5dd9db9
Patrick Villanueva <pkrvillanueva@gmail.com>
2019-12-06 01:40:02 +0800

    Create endpoint for bulk order

--- includes/rest-api/core/works.php
+++ includes/rest-api/core/works.php
@@ -18,38 +18,59 @@ class Works {
 	 * Register routes.
 	 */
 	public function register_routes() {
-		register_rest_route( 'bzalpha/v1', '/pool', [
+		register_rest_route( 'bzalpha/v1', '/order/bulk', [
 			[
 				'methods'  => 'POST',
-				'callback' => [ $this, 'create_pool' ],
+				'callback' => [ $this, 'create_bulk_order' ],
 			]
 		] );
 	}
 
 	/**
-	 * Create pool.
+	 * Create order.
 	 */
-	public function create_pool( $request ) {
+	public function create_bulk_order( $request ) {
 		if ( ! function_exists( 'acf' ) ) {
 			return new \WP_Error( 'invalid_route', 'Invalid route.', [ 'status' => 404 ] );
 		}
 
-		if ( empty( $request['vessel'] ) || empty( $request['pool'] ) || empty( $request['sign_on'] ) || empty( $request['sign_off'] ) ) {
+		if ( empty( $request['vessel'] ) || ! is_array( $request['positions' ] ) || empty( $request['positions'] ) || empty( $request['sign_on'] ) ) {
 			return new \WP_Error( 'invalid_params', 'Invalid params.', [ 'status' => 404 ] );
 		}
 
-		// Update vessel pool.
-		update_field( 'pool', $request['pool'], $request['vessel'] );
+		$data = [];
 
-		// Update each seaman status.
-		foreach ( $request['pool'] as $seaman ) {
-			$seaman = intval( $seaman );
+		$meta_fields = [
+			'vessel',
+			'wage',
+			'currency',
+			'port',
+			'uniform',
+			'sign_on',
+			'deadline',
+			'contract_plus',
+			'contract_minus',
+			'remark',
+		];
 
-			update_field( 'job_status', 'onboard', $seaman );
-			update_field( 'vessel', intval( $request['vessel'] ), $seaman );
-			update_field( 'sign_on', $request['sign_on'], $seaman );
-			update_field( 'sign_off', $request['sign_off'], $seaman );
+		foreach ( $request['positions'] as $position ) {
+			$post_id = wp_insert_post( [
+				'post_status' => 'publish',
+				'post_type'   => 'order',
+			] );
+
+			foreach ( $meta_fields as $meta ) {
+				if ( isset( $request[ $meta ] ) ) {
+					update_field( $meta, $request[ $meta ], $post_id );
+				}
+			}
+
+			update_field( 'status', 'pending', $post_id );
+			update_field( 'position', $position, $post_id );
+			$data[] = get_post( $post_id );
 		}
+
+		return new WP_REST_Response( $data, 200 );
 	}
 }
 
