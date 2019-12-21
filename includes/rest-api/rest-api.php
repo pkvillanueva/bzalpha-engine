9ec10b05d168034638aef67eb953bd0a950884e3
Patrick Villanueva <pkrvillanueva@gmail.com>
2019-12-22 04:21:17 +0800

    Create dynamic rest fields

--- includes/rest-api/rest-api.php
+++ includes/rest-api/rest-api.php
@@ -6,7 +6,8 @@ namespace BZAlpha\REST_API;
  * Setup overrides.
  */
 function setup() {
-    add_filter( 'rest_authentication_errors', __NAMESPACE__ . '\rest_auth' );
+	add_filter( 'rest_authentication_errors', __NAMESPACE__ . '\rest_auth' );
+	add_action( 'rest_api_init', __NAMESPACE__ . '\rest_fields', 10 );
 
     // Include files.
     $rest_includes = BZALPHA_INC . 'rest-api/';
@@ -44,4 +45,59 @@ function rest_auth( $result ) {
  */
 function rest_forbidden() {
 	return new \WP_Error( 'rest_forbidden', __( 'Sorry, you are not allowed to do that.', 'bzalpha' ), [ 'status' => rest_authorization_required_code() ] );
+}
+
+/**
+ * Register rest fields in post types.
+ */
+function rest_fields() {
+	if ( ! function_exists( 'acf' ) ) {
+		return;
+	}
+
+	$post_types = [
+		'bz_order',
+		'vessel',
+		'seaman',
+	];
+
+	foreach ( $post_types as $post_type ) {
+		$fields = \bzalpha_get_fields_map( $post_type );
+
+		if ( $fields ) {
+			foreach ( $fields as $meta_name => $field_key ) {
+				register_rest_field( $post_type, $meta_name, [
+					'schema'       => null,
+					'get_callback' => function() use ( $field_key ) {
+						return \bzalpha_get_field( $field_key );
+					},
+					'update_callback' => function( $value, $post ) use ( $field_key ) {
+						return \bzalpha_update_field( $field_key, $value, $post->ID );
+					}
+				] );
+			}
+		}
+	}
+
+	$taxonomies = [
+		'principal',
+	];
+
+	foreach ( $taxonomies as $tax_name ) {
+		$fields = \bzalpha_get_fields_map( $tax_name );
+
+		if ( $fields ) {
+			foreach ( $fields as $meta_name => $field_key ) {
+				register_rest_field( $tax_name, $meta_name, [
+					'schema'       => null,
+					'get_callback' => function( $taxonomy ) use ( $field_key ) {
+						return \bzalpha_get_field( $field_key, $taxonomy['taxonomy'] . '_' . $taxonomy['id'] );
+					},
+					'update_callback' => function( $value, $taxonomy ) use ( $field_key ) {
+						return \bzalpha_update_field( $field_key, $value, $taxonomy );
+					}
+				] );
+			}
+		}
+	}
 }
