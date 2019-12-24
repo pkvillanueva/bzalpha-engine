bc1cbef75ee3a1c6bc311507e565b98aea1207ed
Patrick Villanueva <patrick@artbees.net>
2019-12-24 15:05:21 +0800

    Move works

--- includes/rest-api/rest-api.php
+++ includes/rest-api/rest-api.php
@@ -14,7 +14,7 @@ function setup() {
     require_once $rest_includes . 'meta-schema.php';
     require_once $rest_includes . 'posts-base.php';
     require_once $rest_includes . 'terms-base.php';
-    require_once $rest_includes . 'core/works.php';
+    require_once $rest_includes . 'works.php';
     require_once $rest_includes . 'seaman.php';
     require_once $rest_includes . 'vessel.php';
     require_once $rest_includes . 'principal.php';
